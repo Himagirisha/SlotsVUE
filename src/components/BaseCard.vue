57b0bc7b25e519410a802a8ca8d93598616fea2e
Himagirisha Dharmaprakash <107013@tfcorp.com>
2022-09-27 16:59:46 +0530

    slots

---
+++ src/components/BaseCard.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+      <slot></slot>
+    </div>
+    <body>
+        <slot name="header"></slot>
+    </body>
+  </template>
+  
+  <script>
+  export default {};
+  </script>
+  
+  <style scoped>
+  div {
+    box-shadow: 0 3px 9px rgba(0, 0, 0, 0.3);
+    border-radius: 10px;
+    max-width: 30rem;
+    background-color: rgb(232, 166, 42);
+    color: whitesmoke;
+  }
+body{
+    border: 1px solid green;
+}
+  </style>
---
+++ src/components/FruitsList.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <base-card>
+      <ul>
+        <li>apple</li>
+        <li>mango</li>
+        <li>grapes</li>
+      </ul>
+    </base-card>
+  </section>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style>
+ul {
+  list-style: none;
+}
+</style>
